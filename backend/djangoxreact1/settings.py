9bda0975297b5eafa6e1586728c9e60fa89db39f
DenisTsh01 <98765891+DenisTsh01@users.noreply.github.com>
2023-11-06 06:31:50 +0200

    baza de date link

--- backend/djangoxreact1/settings.py
+++ backend/djangoxreact1/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'djangoxreact1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('railway'),
+        'USER': os.environ.get('postgres'),
+        'PASSWORD': os.environ.get('F3Be-F43g3d5c4D1adDC2e2D-4DbBgFB'),
+        'HOST': os.environ.get('monorail.proxy.rlwy.net'),
+        'PORT': os.environ.get('37781'),
     }
 }
 
