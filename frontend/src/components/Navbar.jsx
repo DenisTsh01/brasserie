36cb058ee1982d16bc598a6872ac003dc28230e8
DenisTsh01 <98765891+DenisTsh01@users.noreply.github.com>
2023-11-09 00:11:53 +0200

    navbar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'; 
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -28,15 +30,16 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            {windowWidth < 350
+            {
+            windowWidth < 350
               ? "Brasserie"
               : windowWidth < 550
               ? "Caffe des Sports"
               : "Brasserie Caffe des Sports"}
-            <i className='fab fa-typo3' />
+           
           </Link>
           <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            <FontAwesomeIcon icon={click ?    faXmark: faBars } />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
